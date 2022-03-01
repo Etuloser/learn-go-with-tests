4ab9f36728fff3aea8ad6ace5a77cf0968a58bb2
Eles <1494136313@qq.com>
2022-03-01 09:19:27 +0800

    chore: 修改file-upload模块路由

--- file-upload/main.go
+++ file-upload/main.go
@@ -9,13 +9,6 @@ import (
 	"gopkg.in/ini.v1"
 )
 
-// 模拟一些私人数据
-var secrets = gin.H{
-	"foo":    gin.H{"email": "foo@bar.com", "phone": "123433"},
-	"austin": gin.H{"email": "austin@example.com", "phone": "666"},
-	"lena":   gin.H{"email": "lena@guapa.com", "phone": "523443"},
-}
-
 func main() {
 	cfg, err := ini.Load("my.ini")
 	if err != nil {
@@ -31,18 +24,6 @@ func main() {
 	authorized := r.Group("/", gin.BasicAuth(gin.Accounts{
 		username: password,
 	}))
-
-	// /admin/secrets 端点
-	// 触发 "localhost:8080/admin/secrets
-	authorized.GET("/secrets", func(c *gin.Context) {
-		// 获取用户，它是由 BasicAuth 中间件设置的
-		user := c.MustGet(gin.AuthUserKey).(string)
-		if secret, ok := secrets[user]; ok {
-			c.JSON(http.StatusOK, gin.H{"user": user, "secret": secret})
-		} else {
-			c.JSON(http.StatusOK, gin.H{"user": user, "secret": "NO SECRET :("})
-		}
-	})
 
 	authorized.POST("/upload", func(c *gin.Context) {
 
